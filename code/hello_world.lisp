b1cad5e8c5a36ba5745fce0f0c208156070c6e19
werzk <hughman96@icloud.com>
2017-08-17 21:15:18 -0500

    made a list

--- code/hello_world.lisp
+++ code/hello_world.lisp
@@ -9,3 +9,9 @@
 
  (defun triple (x)
 	  (* x x x))
+(list :a 1 :b 2 :c 3)
+(getf (list :a 1 :b 2 :c 3) :a)
+(getf (list :a 1 :b 2 :c 3) :b)
+(defun make-cd (title artist rating ripped)
+	   (list :title title :artist artist :rating rating :ripped ripped))
+(make-cd "culture" "migos" 10 t)
